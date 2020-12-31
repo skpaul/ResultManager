e2dc3597e233b7462bf2251ad0e44f860f2dc7d3
Saumitra Paul <skpaul@gmail.com>
2020-12-31 14:49:51 +0600

    Update result_manager.sql

--- result_manager.sql
+++ result_manager.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 30, 2020 at 09:06 PM
+-- Generation Time: Dec 31, 2020 at 09:49 AM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.11
 
